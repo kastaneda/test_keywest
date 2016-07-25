ebaad23d1261817dd6ade13d75cd27bc24cf2fc5
Dmitry Kolesnikov <kastaneda@gmail.com>
2016-07-25 06:53:54 +0300

    Add 'get bookmark by URL' method

--- www/index.php
+++ www/index.php
@@ -8,11 +8,39 @@ $app['debug'] = true;
 $app->register(new Silex\Provider\DoctrineServiceProvider(),
     include __DIR__ . '/../config.php');
 
-$app->get('/comments-api/v1/latest', function() use ($app) {
-    $sql = "SELECT * FROM bookmark ORDER BY created_at DESC LIMIT 0,10;";
-    $data = $app['db']->fetchAll($sql, array((int) $id));
+$app->get('/v1/bookmark/latest', function () use ($app) {
+    $sql = "SELECT * FROM bookmark ORDER BY created_at DESC LIMIT 0,10";
+    $data = $app['db']->fetchAll($sql);
 
     return $app->json($data);
 }); 
+
+$app->get('/v1/bookmark/{url}', function ($url) use ($app) {
+    $sql = 'SELECT * FROM bookmark WHERE url = ?';
+    $data = $app['db']->fetchAssoc($sql, [$url]);
+    if ($data) {
+        $sql = 'SELECT id, created_at, ip, `text` FROM comment WHERE bookmark_id = ?';
+        $data['comments'] = $app['db']->fetchAll($sql, [$data['id']]);
+        return $app->json($data);
+    } else {
+        $app->abort(404);
+    }
+})->assert('url', 'http.+');
+
+$app->put('/v1/bookmark/{url}', function ($url) use ($app) {
+    $app->abort(403); // TBD
+})->assert('url', 'http.+');
+
+$app->post('/v1/bookmark/{id}', function ($id) use ($app) {
+    $app->abort(403); // TBD
+})->assert('id', '\d+');
+
+$app->put('/v1/comment/{id}', function ($id) use ($app) {
+    $app->abort(403); // TBD
+})->assert('id', '\d+');
+
+$app->delete('/v1/comment/{id}', function ($id) use ($app) {
+    $app->abort(403); // TBD
+})->assert('id', '\d+');
 
 $app->run(); 
