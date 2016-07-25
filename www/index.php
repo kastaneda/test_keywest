861153ba1b147eab137b978fe74057a367240d06
Dmitry Kolesnikov <kastaneda@gmail.com>
2016-07-25 04:49:59 +0300

    Improve vhost configuration

--- www/index.php
+++ www/index.php
@@ -3,9 +3,14 @@
 require_once __DIR__.'/../vendor/autoload.php'; 
 
 $app = new Silex\Application(); 
+$app['debug'] = true;
 
 $app->get('/', function() use($app) { 
     return 'Hello, Silex!'; 
 }); 
+
+$app->get('/hello/{name}', function($name) use ($app) {
+    return 'Hello ' . $app->escape($name);
+});
 
 $app->run(); 
